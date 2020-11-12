b1eb97c675d7e0cd97620f2278deb2ff9972172a
RustyJ4ames <stefan@grafikcentralen.se>
2020-11-12 09:45:41 +0100

    Working cleanslate

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,7 +1,37 @@
-/**
- * Implement Gatsby's Node APIs in this file.
- *
- * See: https://www.gatsbyjs.com/docs/node-apis/
- */
+require("dotenv").config({
+    path: `.env.development`,
+  })
+require("dotenv").config({
+    path: `.env.${process.env.NODE_ENV}`,
+  })
+const path = require(`path`)
 
-// You can delete this file if you're not using it
+exports.createPages = ({ graphql, actions }) => {
+  const { createPage } = actions
+  return graphql(`
+    {
+      allWpPage(sort: { fields: [date] }) {
+        nodes {
+          title
+          excerpt
+          content
+          slug
+        }
+      }
+    }
+  `).then((result) => {
+    //highlight-start
+    result.data.allWpPage.nodes.forEach((node) => {
+      createPage({
+        path: node.slug,
+        component: path.resolve(`./src/templates/blog-post.js`),
+        context: {
+          // This is the $slug variable
+          // passed to blog-post.js
+          slug: node.slug,
+        },
+      })
+    })
+    //highlight-end
+  })
+}
